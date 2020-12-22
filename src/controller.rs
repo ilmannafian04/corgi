f85f251d6dbe723fb6b60745596ebcf0f00e5b6d
Ilman Nafian <milmannafian04@gmail.com>
2020-12-22 14:22:46 +0700

    add base handler for shorten

--- src/controller.rs
+++ src/controller.rs
@@ -1,5 +1,15 @@
 use actix_files::NamedFile;
-use actix_web::{web, Error, HttpResponse};
+use actix_web::{web, HttpResponse};
+use diesel::{
+    r2d2::{ConnectionManager, Pool},
+    PgConnection,
+};
+use rand::{distributions::Alphanumeric, prelude::*, thread_rng};
+use serde::Deserialize;
+
+use crate::models::{Link, NewLink};
+
+type DbPool = Pool<ConnectionManager<PgConnection>>;
 
 pub async fn ping() -> HttpResponse {
     HttpResponse::Ok().body("pong")
@@ -10,6 +20,35 @@ pub async fn index(tmpl: web::Data<tera::Tera>) -> HttpResponse {
     HttpResponse::Ok().body(body)
 }
 
-pub async fn favicon() -> Result<NamedFile, Error> {
+pub async fn favicon() -> std::io::Result<NamedFile> {
     Ok(NamedFile::open("./static/favicon.ico")?)
+}
+
+#[derive(Deserialize)]
+pub struct ShortenParam {
+    link: String,
+}
+
+pub async fn shorten(
+    pool: web::Data<DbPool>,
+    param: web::Form<ShortenParam>,
+    tmpl: web::Data<tera::Tera>,
+) -> HttpResponse {
+    let conn = pool.get().expect("cannot get the DB pool");
+    let new_link = NewLink {
+        shortened: thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect(),
+        original: param.link.to_owned(),
+    };
+    let link = web::block(move || Link::insert_link(&conn, &new_link)).await;
+    match link {
+        Ok(link) => {
+            let body = tmpl.render("shorten.html", &tera::Context::new()).unwrap();
+            HttpResponse::Ok().body(body)
+        }
+        Err(e) => HttpResponse::NotFound().body(format!("{}", e)),
+    }
 }
--- src/route.rs
+++ src/route.rs
@@ -6,6 +6,7 @@ pub fn route_cfg(cfg: &mut web::ServiceConfig) {
     cfg.service(web::scope("/api").route("/ping", web::get().to(c::ping)))
         .service(
             web::scope("")
+                .route("/shorten", web::post().to(c::shorten))
                 .route("/favicon.ico", web::get().to(c::favicon))
                 .route("/", web::get().to(c::index)),
         );
